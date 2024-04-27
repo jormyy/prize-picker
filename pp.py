fce53d24476d67998cc67ef084f1a7114104194a
Jeremy Ha <jorm@Jeremys-MacBook-Pro.local>
2024-04-27 14:29:44 -0700

    created basic files

---
+++ pp.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+import json
+
+
