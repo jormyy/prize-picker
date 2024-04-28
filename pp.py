8f00be7f7608529915f5e0b28a56958b1c7c4e47
Jeremy Ha <jorm@Jeremys-MacBook-Pro.local>
2024-04-27 19:21:47 -0700

    get all data

--- pp.py
+++ pp.py
@@ -7,6 +7,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 import json
+import datetime
 
 
 URL = 'https://app.prizepicks.com/'
@@ -15,6 +16,19 @@ DRIVER = webdriver.Firefox()
 DRIVER.get(URL)
 
 PICKS = []
+
+TODAY = datetime.datetime.now()
+DAY_OF_WEEK = TODAY.strftime("%A")
+
+DAY_HASH = {
+    'Sun': 'Sunday',
+    'Mon': 'Monday',
+    'Tue': 'Tuesday',
+    'Wed': 'Wednesday',
+    'Thu': 'Thursday',
+    'Fri': 'Friday',
+    'Sat': 'Saturday'
+}
 
 
 def setup():
@@ -28,23 +42,22 @@ def setup():
             continue
 
 
-def look_at_sports():
-    sports_div = DRIVER.find_element(By.ID, 'scrollable-area')
-    all_sports = sports_div.find_elements(By.CLASS_NAME, 'league')
-    
-    for sport in all_sports:
-        sport.click()
+def look_at_leagues():
+    leagues_div = DRIVER.find_element(By.ID, 'scrollable-area')
+    all_leagues = leagues_div.find_elements(By.CLASS_NAME, 'league')
+    for league in all_leagues:
+        league.click()
         
         time.sleep(1)
         
-        sport_dict = {}
-        sport_name = sport.find_element(By.CLASS_NAME, 'name').text
-        sport_dict['sport'] = sport_name
+        league_dict = {}
+        league_name = league.find_element(By.CLASS_NAME, 'name').text
+        league_dict['league'] = league_name
         
         stats_div = DRIVER.find_element(By.CLASS_NAME, 'stat-container')
         all_stats = stats_div.find_elements(By.CLASS_NAME, 'stat')
         
-        sport_dict['stats'] = {}
+        league_dict['stats'] = {}
         
         stat_names = []
         
@@ -58,12 +71,11 @@ def look_at_sports():
             stats[i].click()
             # time.sleep(1)
             stat_name = stat_names[i]
-            print(stat_name)
             
             if stat_name == 'Popular 🔥':
                 continue
             
-            sport_dict['stats'][stat_name] = {}
+            league_dict['stats'][stat_name] = {}
             
             players_div = DRIVER.find_element(By.ID, 'projections')
             all_players = players_div.find_elements(By.TAG_NAME, 'li')
@@ -74,55 +86,49 @@ def look_at_sports():
                 line = None
                 
                 try:
-                    line = player.find_element(By.CLASS_NAME, 'text-atlien-100').text
+                    player.find_element(By.CLASS_NAME, 'text-atlien-100').text
+                    continue
                 except NoSuchElementException:
                     line = player.find_element(By.CLASS_NAME, 'flex.flex-1.items-center.pr-2').text
                 
-                if player_name in sport_dict['stats'][stat_name]:
-                    sport_dict['stats'][stat_name][player_name].append(line)
-                else:
-                    sport_dict['stats'][stat_name][player_name] = [line]
-                    
-        PICKS.append(sport_dict)
-        break
-            
-        
-        # for stat in all_stats:
-        #     stat.click()
-        #     stat_name = stat.text
-        #     print(stat_name)
-            
-        #     if stat_name == 'Popular 🔥':
-        #         continue
-            
-        #     # stat.click()
-            
-        #     sport_dict['stats'][stat_name] = {}
-            
-        #     players_div = DRIVER.find_element(By.ID, 'projections')
-        #     all_players = players_div.find_elements(By.TAG_NAME, 'li')
-            
-        #     for player in all_players:
-        #         player_name = player.find_element(By.TAG_NAME, 'h3').text
+                try:
+                    player.find_element(By.CLASS_NAME, 'h-9.w-9.rotate-12')
+                    continue
+                except NoSuchElementException:
+                    line = player.find_element(By.CLASS_NAME, 'flex.flex-1.items-center.pr-2').text
                 
-        #         line = None
+                league_dict['stats'][stat_name][player_name] = [line]
                 
-        #         try:
-        #             line = player.find_element(By.CLASS_NAME, 'text-atlien-100').text
-        #         except NoSuchElementException:
-        #             line = player.find_element(By.CLASS_NAME, 'flex.flex-1.items-center.pr-2').text
+                other = {}
                 
-        #         if player_name in sport_dict['stats'][stat_name]:
-        #             sport_dict['stats'][stat_name][player_name].append(line)
-        #         else:
-        #             sport_dict['stats'][stat_name][player_name] = [line]
-        
-        PICKS.append(sport_dict)
+                team = player.find_element(By.ID, 'test-team-position').text.split()[0]
+                other['team'] = team
+                
+                try:
+                    player.find_element(By.CLASS_NAME, 'text-spottieOttie')
+                    other['day'] = DAY_OF_WEEK
+                except NoSuchElementException:
+                    day = player.find_element(By.TAG_NAME, 'time').text.split()[-2]
+                    other['day'] = DAY_HASH[day]
+                
+                img_div = player.find_element(By.TAG_NAME, 'picture')
+                img = img_div.find_element(By.TAG_NAME, 'img')
+                img_src = img.get_attribute('src')
+                
+                other['img_src'] = img_src
+                
+                league_dict['stats'][stat_name][player_name].append(other)
                     
-                    
+        PICKS.append(league_dict)
+
+
+def format_stats():
+    out_file = open("picks.json", "w") 
+    json.dump(PICKS, out_file, indent = 4) 
+    out_file.close()
+    
+                             
 setup()
-look_at_sports()
-out_file = open("pick.json", "w") 
-json.dump(PICKS, out_file, indent = 6) 
-out_file.close()
+look_at_leagues()
+format_stats()
 DRIVER.quit()
